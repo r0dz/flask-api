b5da8fe8773c5ed5e8233ce70e4de00a97c38225
Rodrigo Siebra <rosiebra@gmail.com>
2023-01-12 21:59:03 -0300

    added database library

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,9 @@
-from flask import Flask, jsonify
+from flask import Flask
+from flask_marshmallow import Marshmallow
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object('config')
-
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
 from .routes import routes
